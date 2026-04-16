94ea8f7df78c2fa1b24c5ee3e35ede5b5fa4f6eb
Kyroven-code <liamlorin54306@gmail.com>
2026-04-16 22:27:15 +0200

    Refactor app.py for improved structure and clarity

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -198,5 +198,6 @@ def get_recherche():
 #  LANCEMENT
 # ============================================================
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
