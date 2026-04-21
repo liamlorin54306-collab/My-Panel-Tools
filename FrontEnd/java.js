e42ae2be6d63e4a714f3e7ac648e08554998d99b
Kyroven-code <liamlorin54306@gmail.com>
2026-04-21 19:09:06 +0200

    Update java.js

--- FrontEnd/java.js
+++ FrontEnd/java.js
@@ -542,8 +542,6 @@ async function traduireTexte() {
     el.value = 'Traduction en cours...'
 
     try {
-        // Détecter la langue source (fr par défaut)
-        const de = 'fr'
         const langPair = `${de}|${vers}`
         const res = await fetch(
             `https://api.mymemory.translated.net/get?q=${encodeURIComponent(texte)}&langpair=${langPair}`
